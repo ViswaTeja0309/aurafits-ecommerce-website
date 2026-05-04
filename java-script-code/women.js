4e4ef177e5336196fcc86a5171ddec65dfc726a3
Gujjula Viswa Teja <viswatejagujjula@gmail.com>
2026-05-04 23:13:24 +0530

    Fixed saree image path

--- java-script-code/women.js
+++ java-script-code/women.js
@@ -8,7 +8,7 @@ const dynamicProducts = document.querySelector(".dynamic-products");
 const saree_products = [
 
     {
-        image:"project_images/women's wear/sarees/Sarees.jpg",
+        image:"project_images/women's wear/sarees/saree-2.jpg",
         title:"Royal Silk Elegance",
         price:"Rs.3,999"
     },
